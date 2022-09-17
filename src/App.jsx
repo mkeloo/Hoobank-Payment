1131a15c25acc303747eddd30e8ae3e82e5d90a0
mkeloo <mkeloo@ufl.edu>
2022-09-17 05:38:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './style';
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}> Hero </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
