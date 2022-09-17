247389c7f22ddfa5a578256c8878f17333468a39
mkeloo <mkeloo@ufl.edu>
2022-09-17 19:59:22 -0400

    card deal done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
-const Billing = () => {
-  return <div>Billing</div>;
-};
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        <span className="link link-underline link-underline-black">
+          Easily control your
+        </span>
+        <br className="sm:block hidden" />
+        <span className="link link-underline link-underline-black">
+          {' '}
+          billing & invoicing
+        </span>
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img
+          src={apple}
+          alt="google_play"
+          className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer hover:scale-110 duration-300"
+        />
+        <img
+          src={google}
+          alt="google_play"
+          className="w-[144.17px] h-[43.08px] object-contain cursor-pointer  hover:scale-110 duration-300"
+        />
+      </div>
+    </div>
+  </section>
+);
 
 export default Billing;
